109278de07b0720e3d72ff6034664863a322a095
Jay Hoffmann <jhoffm34@gmail.com>
2017-06-26 10:41:04 -0400

    Adding comments throughout and retrieving layouts from field

--- cmb2-flexible-content-field.php
+++ cmb2-flexible-content-field.php
@@ -7,14 +7,29 @@
  * Author: Reaktiv Studios
  * Author URI: https://reaktivstudios.com
  * License: GPLv2+
+ *
+ * @package  cmb2-flexible-content-field
  */
 
-if ( ! class_exists( 'RKV_CMB2_Flexible_Content_Field', false  ) ) {
+if ( ! class_exists( 'RKV_CMB2_Flexible_Content_Field', false ) ) {
 
+	/**
+	 * Flexible content field render and sanitization callbacks
+	 */
 	class RKV_CMB2_Flexible_Content_Field {
 
+		/**
+		 * Constain instance of class
+		 *
+		 * @var RKV_CMB2_Flexible_Content_Field
+		 */
 		protected static $instance;
 
+		/**
+		 * Set up static instance of class
+		 *
+		 * @return class RKV_CMB2_Flexible_Content_Field instance
+		 */
 		public static function get_instance() {
 			if ( ! isset( static::$instance ) && ! ( self::$instance instanceof RKV_CMB2_Flexible_Content_Field ) ) {
 				static::$instance = new RKV_CMB2_Flexible_Content_Field();
@@ -23,85 +38,81 @@ if ( ! class_exists( 'RKV_CMB2_Flexible_Content_Field', false  ) ) {
 			return static::$instance;
 		}
 
+		/**
+		 * Add hooks and filters to flexible content field
+		 */
 		private function init() {
 			add_action( 'cmb2_render_flexible', array( $this, 'render_fields' ), 10, 5 );
 			add_filter( 'cmb2_sanitize_flexible', array( $this, 'save_fields' ), 12, 4 );
 		}
 
+		/**
+		 * Render fields callback
+		 *
+		 * Grabs the layouts from the field data and the current data from the database,
+		 * then constructs a new field group for each used layout and renders that out, one by one.
+		 *
+		 * Groups are used so that the CMB2 API can automatically apply
+		 * necessary render functions for each individual field.
+		 *
+		 * The data for each group needs to be overrrdden though since it is stored in a different type of array.
+		 *
+		 * @param  object $field         Field arguments and parameters.
+		 * @param  array  $escaped_value Escaped value from database.
+		 * @param  int    $object_id     Integer for full object.
+		 * @param  string $object_type  Object type.
+		 * @param  string $field_type    Field type.
+		 */
 		public function render_fields( $field, $escaped_value, $object_id, $object_type, $field_type ) {
 			$metabox = $field->get_cmb();
+			$layouts = isset( $field->args['layouts'] ) ? $field->args['layouts'] : false;
 
-			$layouts = array(
-				'text' => array(
-					'title' => 'Text Group',
-					'fields' => array(
-						array(
-							'type' => 'text',
-							'name' => 'Title for Text Group',
-							'id' => 'title',
-						),
-						array(
-							'type' => 'textarea',
-							'name' => 'Description for Text Group',
-							'id' => 'description',
-						)
-					),
-				),
-				'image' => array(
-					'title' => 'Image Group',
-					'fields' => array(
-						array(
-							'type' => 'image',
-							'name' => 'Image for Image Group',
-							'id' => 'title',
-						),
-						array(
-							'type' => 'textarea',
-							'name' => 'Description for Image Group',
-							'id' => 'description',
-						)
-					),
-				),
-			);
+			if ( false === $layouts ) {
+				// We need layouts for this to work.
+				return false;
+			}
 
+			// Stubbed data.
+			// This is what data should look like when getting it from the database.
 			$data = array(
 				array(
 					'layout' => 'text',
 					'values' => array(
 						'title' => 'title value',
-						'description' => 'description value'
+						'description' => 'description value',
 					),
 				),
 				array(
 					'layout' => 'text',
 					'values' => array(
 						'title' => 'title value 2',
-						'description' => 'description value 2'
+						'description' => 'description value 2',
 					),
-				)
+				),
 			);
 
-			// Store these so tehy can accessed in the hook
+			// Store these so tehy can accessed in the hook.
 			$this->stored_data = $data;
-			$prefix = $field->_id() . '_';
-			$this->prefix = $prefix;
 
-			foreach( $data as $i => $group ) {
+			$field_id = $field->_id();
+			foreach ( $data as $i => $group ) {
 				$layout_data = $layouts[ $group['layout'] ];
 				$layout_fields = $layout_data['fields'];
-				$group_id = $prefix . $i;
-				$group_id = $field->_id() . '[' . $i . ']';
+				$group_id = $field_id . '[' . $i . ']';
 
+				// Create a new group that will hold the layout group.
+				// Make sure to define the ID as an array so that it is passed to the sanitization callback
+				// The array_key should be defined on both main group field and all subfields.
 				$group_args = array(
 					'id' => $field->_id() . '[' . $i . ']',
 					'type' => 'group',
 					'array_key' => absint( $i ),
 				);
-
 				$group_name = $metabox->add_field( $group_args );
 				$group_args['fields'] = array();
 
-				foreach( $layout_fields as $subfield ) {
+				// Foreach layout field, add a field to the group.
+				foreach ( $layout_fields as $subfield ) {
 					$subfield_args = array(
 						'id' => $subfield['id'],
 						'type' => $subfield['type'],
@@ -112,63 +123,27 @@ if ( ! class_exists( 'RKV_CMB2_Flexible_Content_Field', false  ) ) {
 					$group_args['fields'][ $subfield['id'] ] = $subfield_args;
 				}
 
+				// Set some necessary defaults.
 				$group_args['context'] = 'normal';
 				$group_args['show_names'] = true;
 
-				add_filter( 'cmb2_override_' . $group_id . '_meta_value', array( $this, 'correct_value'), 10, 4 );
+				add_filter( 'cmb2_override_' . $group_id . '_meta_value', array( $this, 'override_meta_value' ), 10, 4 );
 				$metabox->render_group( $group_args );
-				// remove_filter( 'cmb2_override_' . $group_id . '_meta_value', array( $this, 'correct_value' ) );
+				remove_filter( 'cmb2_override_' . $group_id . '_meta_value', array( $this, 'override_meta_value' ) );
 			}
-
-			// $prefix = $field->_id() . '_';
-
-			// foreach( $data as $group_id => $layout ) {
-			// 	$layout_data = $layouts[ $layout ];
-			// 	$layout_fields = $layout_data['fields'];
-
-
-			// 	// Create an initial Group based on our data.
-			// 	$group_args = array(
-			// 		'id' => $prefix . $group_id,
-			// 		'type' => 'group'
-			// 	);
-			// 	$create_group = $metabox->add_field( $group_args );
-			// 	$group_args['fields'] = array();
-
-
-
-
-			// 	// Get the fields that are needed for this group and add each of them as a subfield on the group.
-			// 	foreach ( $layout_fields as $subfield ) {
-			// 		$subfield_args = array(
-			// 			'id' => $subfield['id'],
-			// 			'type' => $subfield['type'],
-			// 			'name' => $subfield['name']
-			// 		);
-
-			// 		$subfield_id = $metabox->add_group_field( $create_group, $subfield_args );
-			// 		$group_args['fields'][ $subfield['id'] ] = $subfield_args;
-			// 	}
-
-
-
-
-			// 	// Add some default settings and render it all out to the page.
-			// 	$group_args['context'] = 'normal';
-			// 	$group_args['show_names'] = true;
-
-			// 	// Get all post meta
-			// 	// Add some sort of filter
-			// 	// Filter each one
-				
-			// 	add_filter( 'cmb2_override_my-prefix_flexible_first_group_id_meta_value', array( $this, 'correct_value' ), 10, 4 );
-			// 	$metabox->render_group( $group_args );
-			// }
-
 
 		}
 
-		public function correct_value( $data, $object_id, $a, $object ) {
+		/**
+		 * Override the meta value for each group with data from database
+		 *
+		 * @param  object $data      Original data set.
+		 * @param  int    $object_id Object ID.
+		 * @param  array  $a         Array of arguments for field.
+		 * @param  object $object    Full field object.
+		 * @return array            Data to save
+		 */
+		public function override_meta_value( $data, $object_id, $a, $object ) {
 			if ( isset( $object->args['array_key'] ) ) {
 
 				$array_key = absint( $object->args['array_key'] );
@@ -178,13 +153,21 @@ if ( ! class_exists( 'RKV_CMB2_Flexible_Content_Field', false  ) ) {
 			return $data;
 		}
 
+		/**
+		 * Sanitization callback
+		 *
+		 * @param  array $override_value Value that's being overridden.
+		 * @param  array $value          Value from post object.
+		 * @param  int   $object_id      Object / field ID.
+		 * @param  array $field_args     Full list of arguments.
+		 * @return string                Data
+		 */
 		public function save_fields( $override_value, $value, $object_id, $field_args ) {
-
 			// Get the value and then sanitize it according to sanitization rules.
 			return '';
 		}
 
 	}
-	
+
 	RKV_CMB2_Flexible_Content_Field::get_instance();
 }
