85558d3f9fab41247a78257ce8ad0e730c4b4a1a
Jay Hoffmann <jhoffm34@gmail.com>
2017-07-05 14:46:13 -0400

    Commenting JS

--- assets/js/cmb2-flexible.js
+++ assets/js/cmb2-flexible.js
@@ -1,10 +1,14 @@
 var cmb_flexible = {};
+window.CMB2 = window.CMB2 || {};
 window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 
 (function( $, cmb ) {
 
 	var l10n = window.cmb2_l10 || {};
 
+	/**
+	 * Iniitalize Flexible content field
+	 */
 	cmb_flexible.init = function() {
 		var $metabox = cmb.metabox();
 
@@ -20,13 +24,23 @@ window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 		} );
 	}
 
+	/**
+	 * Make sure no rows in flexible fields are set to disabled
+	 * Reset iterators whenever a flexible field is removed.
+	 *
+	 * @param  {object} evt Event object.
+	 */
 	cmb_flexible.removeDisabledRows = function( evt ) {
 		var $el = $( evt.target );
 		if (  $el.find( '.cmb-flexible-rows' ).length > 0 ) {
 			$el.find( '.cmb-remove-group-row' ).prop( 'disabled', false );
+			cmb_flexible.resetIterators( $el );
 		}
 	}
 
+	/**
+	 * Add the ability to remove all rows for flexible content fields.
+	 */
 	cmb_flexible.removeLastRow = function() {
 		var el = $(document).find( this );
 
@@ -39,6 +53,14 @@ window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 		}
 	}
 
+	/**
+	 * Send an AJAX request for a new field and then add it to the DOM.
+	 *
+	 * Once a field has been added, need to make sure to initialize its 
+	 * dependencies and WYSIWYG functionality
+	 *
+	 * @param {object} evt Event object.
+	 */
 	cmb_flexible.addFlexibleRow = function( evt ) {
 		evt.preventDefault();
 
@@ -99,11 +121,24 @@ window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 		});
 	}
 
+	/**
+	 * Show list of types on click.
+	 *
+	 * @param  {object} evt Event object.
+	 */
 	cmb_flexible.removeFlexibleHiddenClass = function( evt ) {
 		evt.preventDefault();
-		var list = $( this ).next( '.cmb-flexible-add-list' ).removeClass( 'hidden' );
+		var list = $( this ).next( '.cmb-flexible-add-list' ).toggleClass( 'hidden' );
 	}
 
+	/**
+	 * Sort through inputs and change the name attributes depending
+	 * on new iterator number.
+	 *
+	 * @param  {object} $el     jQuery Element object.
+	 * @param  {int}    prevNum Number to look for.
+	 * @param  {int}    newNum  Number to change to.
+	 */
 	cmb_flexible.updateFlexibleNames = function( $el, prevNum, newNum ) {
 		if ( $el.length > 0 ) {
 			newNum = newNum || prevNum - 1;
@@ -120,6 +155,11 @@ window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 		}
 	}
 
+	/**
+	 * Shift Rows by switching them in the DOM.
+	 *
+	 * @param  {object} evt Event object.
+	 */
 	cmb_flexible.shiftRows = function( evt ) {
 		evt.preventDefault();
 
@@ -149,17 +189,27 @@ window.CMB2.wysiwyg = window.CMB2.wysiwyg || false;
 		cmb_flexible.updateFlexibleNames( $from, fromNum, gotoNum );
 		cmb_flexible.updateFlexibleNames( $goto, gotoNum, fromNum );
 
-		var $table = $this.closest( '.cmb-repeatable-group' );
-		$table.find( '.cmb-repeatable-grouping' ).each( function( rowindex ) {
-			var $row = $( this );
-			$row.data( 'iterator', rowindex );
-			$row.attr( 'data-iterator', rowindex );
-		} );
+		cmb_flexible.resetIterators( $this );
 
 		$from.add( $goto ).each(function() {
 			window.CMB2.wysiwyg.initRow( $( this ) );
 		} );
 
+	}
+
+	/**
+	 * We use both the data-iterator attribute to shift rows,
+	 * so need to make sure that is reset as well.
+	 *
+	 * @param  {object} $el jQuery element.
+	 */
+	cmb_flexible.resetIterators = function( $el ) {
+		var $table = $el.closest( '.cmb-repeatable-group' );
+		$table.find( '.cmb-repeatable-grouping' ).each( function( rowindex ) {
+			var $row = $( this );
+			$row.data( 'iterator', rowindex );
+			$row.attr( 'data-iterator', rowindex );
+		} );
 	}
 
 	$( cmb_flexible.init );
